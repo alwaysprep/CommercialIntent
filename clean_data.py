e40a6d870365fd3b7bda4bfe55cf9cb662193e94
oguzhansagoglu <alwaysprep@gmail.com>
2014-11-26 21:40:08 +0200

    working clean_me function

--- clean_data.py
+++ clean_data.py
@@ -11,6 +11,19 @@ def clean_me(file_name, write_file_name):
 
     w = open(write_file_name,"w")
 
-    #TODO clean and WRİTE to file
+    my_lis = []
+    for line, count in mydic.items():
+        if count>6:
+            count=6
+
+        for c in range(count):
+            my_lis.append(line)
 
 
+    for line in my_lis:
+        w.write(line)
+
+    w.close()
+
+
+clean_me("data.csv", "cleaned_data.csv")
