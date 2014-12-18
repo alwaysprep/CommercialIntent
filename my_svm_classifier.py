08559d1ef5ea114ef0c4ef254cbf9d51f83cc8f3
oguzhansagoglu <alwaysprep@gmail.com>
2014-12-18 21:52:13 +0200

    use pipeline for vectorisation

--- my_svm_classifier.py
+++ my_svm_classifier.py
@@ -1,3 +1,7 @@
+from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import KFold
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
 from sklearn.svm import SVC
 
 from reader import extract
@@ -7,8 +11,30 @@ from reader import extract
 new_data_text, new_data_class = extract("cleaned_data.csv")
 
 
-cls = SVC()
+pipeline = Pipeline([
+    ('vectorizer', CountVectorizer(ngram_range=(1, 1))),
+    ('classifier', SVC(kernel='linear')),
+])
 
-train = new_data_class[:len(new_data_class)/2]
+k_fold = KFold(n=new_data_text.size, n_folds=5)
 
-cls.fit(train)
+
+scores = []
+
+for train_indices, test_indices in k_fold:
+
+    train_text = new_data_text[train_indices]
+    train_y = new_data_class[train_indices]
+    test_text = new_data_text[test_indices]
+    test_y = new_data_class[test_indices]
+
+    pipeline.fit(train_text, train_y)
+
+    predicted = pipeline.predict(test_text)
+
+    score = pipeline.score(test_text, test_y)
+
+    scores.append(score)
+
+
+print(score)
